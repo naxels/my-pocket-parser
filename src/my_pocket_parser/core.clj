8d2e5acbced3e3f3064a0901585177bf1f26d090
Patrick van de Glind <patrick_de_glind@hotmail.com>
2024-02-15 10:58:27 +0100

    Make core.clj work again with 10 items returned
    and a cleanup of fn's moved to other ns's

--- src/my_pocket_parser/core.clj
+++ src/my_pocket_parser/core.clj
@@ -1,69 +1,75 @@
 (ns my-pocket-parser.core
   (:gen-class)
-  (:require [clj-http.client :as http]
-            [clojure.data.json :as json]
-            [my-pocket-parser.helpers :refer :all]
-            [my-pocket-parser.stats :refer :all]))
-
-;; https://getpocket.com/developer/docs/v3/retrieve
-(def pocket-get-url (build-url "https://getpocket.com" "v3" "get"))
-
-(defn create-json-payload
-  [consumer-key access-token]
-  (json/write-str {:consumer_key consumer-key
-                   :access_token access-token
-                   :state "unread"
-                   :count "2"
-                   :detailType "complete"}))
-
-(defn http-retrieve
-  [url request_body]
-  (let [resp (http/post url {:body request_body
-                             :content-type :json})]
-    (when (= 200 (:status resp))
-      (json/read-str (:body resp)
-                     :key-fn keyword))))
-
-;; Testers
-; call retrieve
-(def m (http-retrieve pocket-get-url (create-json-payload (:consumer-key read-config)
-                                                          (:access-token read-config))))
-; (:list m)
-; (get m :list) ; list of items
-(def items (get m :list))
-
-;; convert :time_added into time (is unix timestamp)
-;;(defn get-specific-keys-for-item
-;;  "Return the specific keys for an item"
-;;  [item]
-;;  (select-keys (second item) [:given_url :favorite :time_added]))
-  ;;(get-in (second item) [:given_url]))
-
-;; each key in items
-;; get [:given_url :favorite :time_added]
-;; (def working-map (map get-keys-for-item items))
-
-;; (defn convert-time
-  ;; "Return a new map with :time_added updated to actual timestamp"
-  ;; [datamap]
-  ;; (map :time_added datamap))
-;; probably create a function that extracts the :time_added for each item, then converts the time and puts it back in each item + return
-
-;; (defn sequence-of-events
-  ;; []
-  ;; (-> working-map ; get data, then transform
-      ;; convert-time));TODO-> ))
-;; (transform convert_time working_map) 
+  (:require [my-pocket-parser.utils :as u]
+            [my-pocket-parser.pocket :as p]))
+            ;[my-pocket-parser.stats :as stats]))
 
 (defn -main
   "Grab dataset from Pocket and display stats"
-  [& args]
+  [& _args]
+  (let [p-opts (p/common-required-parameters [(:consumer-key u/read-config)
+                                              (:access-token u/read-config)])
+        retrieve-payload (p/payload p-opts p/retrieve-optional)
+        items (-> (u/http-retrieve p/retrieve retrieve-payload)
+                  (get :list))]
+    (println (str "Found " (count items) " items"))
+    (doseq [[_id item] items]
+      #_(println (keys item))
+      (println (str "- " (:resolved_title item)))))
   ;; (prn (count (sequence-of-events)))) ; temp: print the count of the map
-  (let [i (partial items->seq-for-keyword items)]
-    (do
-      (prn (frequencies (i :status)))
-      (prn (frequencies (i :favorite)))
-      (prn (->>
-            (i :time_added)
-            (map str->int) ; convert to number
-            (map epoch->time)))))) ;convert to timestamp
+  #_(let [i (partial helpers/items->seq-for-keyword items)]
+     (prn (frequencies (i :status)))
+     (prn (frequencies (i :favorite)))
+     (prn (->>
+           (i :time_added)
+           (map helpers/str->int) ; convert to number
+           (map helpers/epoch->time))))) ;convert to timestamp
+
+(comment
+  ;; Testers
+
+  ; call retrieve
+  (def p-opts (p/common-required-parameters [(:consumer-key u/read-config)
+                                             (:access-token u/read-config)]))
+  (def retrieve-payload (p/payload p-opts p/retrieve-optional))
+
+  ;; item {id map}
+  (def items (-> (u/http-retrieve p/retrieve retrieve-payload)
+                 (get :list)))
+
+  items
+
+  (count items)
+
+  ;; {id map}
+  (def item (second (first items)))
+  ;; (clojure.pprint/pprint (second (second items)))
+
+  (keys item)
+
+  item
+
+  ;; convert :time_added into time (is unix timestamp)
+  ;;(defn get-specific-keys-for-item
+  ;;  "Return the specific keys for an item"
+  ;;  [item]
+  ;;  (select-keys (second item) [:given_url :favorite :time_added]))
+    ;;(get-in (second item) [:given_url]))
+
+  ;; each key in items
+  ;; get [:given_url :favorite :time_added]
+  ;; (def working-map (map get-keys-for-item items))
+
+  ;; (defn convert-time
+    ;; "Return a new map with :time_added updated to actual timestamp"
+    ;; [datamap]
+    ;; (map :time_added datamap))
+  ;; probably create a function that extracts the :time_added for each item, then converts the time and puts it back in each item + return
+
+  ;; (defn sequence-of-events
+    ;; []
+    ;; (-> working-map ; get data, then transform
+        ;; convert-time));TODO-> ))
+  ;; (transform convert_time working_map)
+
+  )
